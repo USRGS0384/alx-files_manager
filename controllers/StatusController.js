5f353d3047a2ab81619f6835e8ddd3413c0c0aec
Philip Ajuong Deng <philipajuong@gmail.com>
2024-12-11 23:42:26 -0800

    0x04.Files manager

--- controllers/StatusController.js
+++ controllers/StatusController.js
@@ -1,16 +1,15 @@
-// controllers/StatusController.js
+// controllers/StatsController.js
 
-import redisClient from '../utils/redis.js';
 import dbClient from '../utils/db.js';
 
-class StatusController {
-    static async getStatus(req, res) {
-        const redisStatus = redisClient.isAlive();
-        const dbStatus = dbClient.isAlive();
+class StatsController {
+    static async getStats(req, res) {
+        const userCount = await dbClient.nbUsers();
+        const fileCount = await dbClient.nbFiles();
 
-        res.status(200).json({ redis: redisStatus, db: dbStatus });
+        res.status(200).json({ users: userCount, files: fileCount });
     }
 }
 
-export default StatusController;
+export default StatsController;
 
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ import express from 'express';
 import AuthController from '../controllers/AuthController.js';
 import UserController from '../controllers/UserController.js';
 import FilesController from '../controllers/FilesController.js';
-import StatusController from '../controllers/StatusController.js'; // Import the StatusController
+import StatusController from '../controllers/StatusController.js'; // Import StatusController
+import StatsController from '../controllers/StatsController.js'; // Import StatsController
 
 const router = express.Router();
 
 router.get('/status', StatusController.getStatus); // Define the /status route
+router.get('/stats', StatsController.getStats); // Define the /stats route
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
