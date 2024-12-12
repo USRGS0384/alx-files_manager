f7a7f8776b11953dcd22ca4b0d731bb1b5484d48
Philip Ajuong Deng <philipajuong@gmail.com>
2024-12-11 22:59:49 -0800

    0x04.Files manager

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,24 +1,18 @@
 // controllers/UserController.js
 
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
 
 class UserController {
     static async getMe(req, res) {
         const token = req.headers['x-token'];
-        if (!token) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
+        if (!token) return res.status(401).json({ error: 'Unauthorized' });
 
         const userId = await redisClient.get(`auth_${token}`);
-        if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
+        if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
         const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
-        if (!user) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
+        if (!user) return res.status(401).json({ error: 'Unauthorized' });
 
         return res.status(200).json({ id: user._id, email: user.email });
     }
