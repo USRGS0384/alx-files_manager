7d88fe6d8f377fea73d611e5e88ffec57ffa157a
Philip Ajuong Deng <philipajuong@gmail.com>
2024-12-14 19:11:09 +0100

    0. Redis utils

--- 0-main.js
+++ 0-main.js
@@ -1,16 +1,12 @@
 import redisClient from './utils/redis';
 
 (async () => {
-    console.log(redisClient.isAlive()); // Should log "true" if Redis is connected
+    console.log(redisClient.isAlive());
+    console.log(await redisClient.get('myKey'));
+    await redisClient.set('myKey', 12, 5);
+    console.log(await redisClient.get('myKey'));
 
-    console.log(await redisClient.get('myKey')); // Should log "null" (key does not exist yet)
-
-    await redisClient.set('myKey', 12, 5); // Set "myKey" to 12 with a 5-second expiration
-    console.log(await redisClient.get('myKey')); // Should log "12"
-
-    // Wait 10 seconds, after which the key will have expired
     setTimeout(async () => {
-        console.log(await redisClient.get('myKey')); // Should log "null" (key expired)
-    }, 1000 * 10);
+        console.log(await redisClient.get('myKey'));
+    }, 1000*10)
 })();
-
