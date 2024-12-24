aa7a13d8c13b2d099d73b21dc2db34b666b3b65c
Philip Ajuong Deng <philipajuong@gmail.con>
2024-12-24 03:04:52 -0800

    First API

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import routes from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-controllerRouting(app);
+app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
+
