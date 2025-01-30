dda633aaabb66797cd923d372c8504cb27fcf239
Philip Ajuong Deng <philipajuong@gmail.com>
2025-01-30 18:45:42 +0000

    files manager

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,25 @@ const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+// Ensure that /status route checks Redis and MongoDB status
+router.get('/status', (req, res) => {
+  res.status(200).json({ redis: redisClient.isAlive(), mongo: dbClient.isAlive() });
+});
+
+// Ensure that /stats route retrieves correct counts
+router.get('/stats', async (req, res) => {
+  const usersCount = await dbClient.nbUsers();
+  const filesCount = await dbClient.nbFiles();
+  
+  res.status(200).json({ users: usersCount, files: filesCount });
+});
+
+// Other routes
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
@@ -20,3 +34,4 @@ router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
+
