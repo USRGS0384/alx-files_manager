fdf0c8624dd2aa94b6ca69874e01b4df007c9a99
Philip Ajuong Deng <philipajuong@gmail.com>
2025-02-02 16:10:35 +0000

    files manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,33 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+const redisClient = require("../utils/redis")
+const dbClient = require("../utils/db")
 
 class AppController {
   static getStatus(req, res) {
     const status = {
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    };
-    return res.status(200).json(status);
+    }
+    res.status(200).json(status)
   }
 
   static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    return res.status(200).json(stats);
+    try {
+      const usersCount = await dbClient.nbUsers()
+      const filesCount = await dbClient.nbFiles()
+
+      const stats = {
+        users: usersCount,
+        files: filesCount,
+      }
+
+      res.status(200).json(stats)
+    } catch (error) {
+      console.error("Error getting stats:", error)
+      res.status(500).json({ error: "Internal Server Error" })
+    }
   }
 }
 
-module.exports = AppController;
+module.exports = AppController
+
+
